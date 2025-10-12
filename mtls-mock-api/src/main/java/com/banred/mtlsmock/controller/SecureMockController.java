6f94c4f66d3f0f2dea519171cbbf132fa95d2f2b
WilsonCastro1988 <yagamichester@gmail.com>
2025-10-12 17:12:21 -0500

    update mockTLS

--- mtls-mock-api/src/main/java/com/banred/mtlsmock/controller/SecureMockController.java
+++ mtls-mock-api/src/main/java/com/banred/mtlsmock/controller/SecureMockController.java
@@ -1,19 +1,48 @@
 package com.banred.mtlsmock.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/secure")
 public class SecureMockController {
 
     @GetMapping("/mtls")
-    public ResponseEntity<String> hello(Principal principal) {
+    public ResponseEntity<String> getMtls(Principal principal) {
         return ResponseEntity.ok("✅ Hi from MockTls XD  " + principal.getName() +
                 ", conexión mTLS establecida con éxito!");
     }
+
+    @PostMapping("/mtls")
+    public ResponseEntity<Map<String, Object>> postMtls(@RequestBody String payload, @RequestHeader HttpHeaders headers) {
+        try {
+            Map<String, Object> response = new HashMap<>();
+
+            Map<String, String> headerMap = new HashMap<>();
+            for (Map.Entry<String, List<String>> entry : headers.entrySet()) {
+                headerMap.put(entry.getKey(), entry.getValue().get(0)); // Tomar el primer valor si hay múltiples
+            }
+
+            // Construir respuesta estructurada
+            response.put("status", "✅ Hi AGAIN from MockTls!");
+            response.put("payload", payload);
+            response.put("headers", headerMap);
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("status", "✅ FAIL from MockTls XD");
+            errorResponse.put("error", e.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
+
